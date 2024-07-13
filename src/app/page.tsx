8c1c2201b389840213c8e408ab787df71ab2bb0c
Quelvinmp <117099025+Quelvinmp@users.noreply.github.com>
2024-07-13 18:53:47 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-// import ListaDePresentes from "@/app/components/ListaDePresentes";
+import ListaDePresentes from "@/app/components/ListaDePresentes";
 
-// export default function Home() {
-//   return (
-//     <div className="bg-secondary-content w-full flex flex-col items-center">
-//       <ListaDePresentes />
-//     </div>
-//   )
-// }
+export default function Home() {
+  return (
+    <div className="bg-secondary-content w-full flex flex-col items-center">
+      <ListaDePresentes />
+    </div>
+  )
+}
