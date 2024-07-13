3dfd7b4089924c70c76cfcdb38a9690547b8a551
Quelvinmp <117099025+Quelvinmp@users.noreply.github.com>
2024-07-13 19:39:28 -0300

    Update Hero.jsx

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -13,13 +13,13 @@ export default function Hero() {
             <p>&</p>
             <p>Laura</p>
           </h1>
-          <p className="mb-5 mt-64">Se desejarem nos presentear, a lista de presentes pode ser encontrada nas lojas:</p>
+          <p className="mb-5 mt-48">Se desejarem nos presentear, a lista de presentes pode ser encontrada nas lojas:</p>
           <ul>
             <li>Bazar Opção Utilidades</li>
             <li>Super Louças</li>
             <li>Casa & Cia (Alberto Braune)</li>
           </ul>
-          <p>Agradecemos de coração qualquer carinho! ❤️</p>
+          <p className="mt-5">Agradecemos de coração qualquer carinho! ❤️</p>
           <p className="mt-5 text-xs ">Quer fazer um pix?</p>
           <p className="font-bold btn-link text-xs" onClick={handleClipboard}>Copie a Chave Aqui</p>
         </div>
